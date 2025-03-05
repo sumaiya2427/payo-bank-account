07679fd955f4237a651ded6b1ee0a9d3480b8f88
sumaiya2427 <sumaiya2427@gmail.com>
2025-03-05 21:27:32 +0600

    ak page theke arek page

--- java/login.js
+++ java/login.js
@@ -1,4 +1,21 @@
 // alert();
-document.getElementById("login-btn").addEventListener("click", function () {
-  console.log("hello");
-});
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(event);
+    const accountNumber = document.getElementById("account-number").value;
+    console.log(accountNumber);
+    const pin = document.getElementById("pin").value;
+    console.log(accountNumber, pin);
+    if (accountNumber.length === 11) {
+      console.log("okay thik ace");
+      if (parseInt(pin) === 1234) {
+        window.location.href = "./main.html";
+      } else {
+        alert("pin thik nai");
+      }
+    } else {
+      alert(" need valid account number");
+    }
+  });
